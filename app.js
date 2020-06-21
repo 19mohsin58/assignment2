b04247bd80bf2cf6992faa7460dd6b0de4eab64d
mohsinjokhio320@gmail.com <mohsinjokhio320@gmail.com>
2020-06-21 05:47:35 -0700

    assignment2javascript

---
+++ app.js
@@ -0,0 +1,732 @@
+
+						//CHAPTER 21 To 25
+						//TASK1
+
+// var firstName = prompt("Write Your First Name","First Name")
+// var lastName = prompt("Write Your Last Name","Last Name")
+// var fullName=firstName+" "+lastName
+// alert("Welcome to Our Company Mr : "+fullName)
+
+
+
+						//CHAPTER 21 To 25
+						//TASK2
+
+
+ // var favtMobile = prompt("Write Your Favourite Mobile Model","Mobile Model")
+ // document.write("My Favourite Mobile Model is "+favtMobile)
+ // document.write("<br>") 
+ // var lenghtOFModel=favtMobile.length;
+ // document.write("Length of String is: "+lenghtOFModel)
+
+
+
+
+
+
+						//CHAPTER 21 To 25
+						//TASK3
+
+
+
+// var country="pakistani"
+// var check=	country.indexOf("n")
+// document.write("String is : "+country)
+// document.write("<br>")
+// document.write("index of 'n' is : "+check)
+
+
+
+
+						//CHAPTER 21 To 25
+						//TASK4
+
+
+
+// var world="Hello World"
+// var segIndex = world.lastIndexOf("l");
+// document.write("String : "+world)
+// document.write("<br>")
+// document.write("Index of last 'l' is : "+segIndex)
+
+
+
+
+
+
+						//CHAPTER 21 To 25
+						//TASK5
+
+
+
+// var numString="Pakistani"
+// var numIndex = numString.charAt(3)
+
+// document.write("String : "+numString)
+// document.write("<br>")
+// document.write("Character at Index 3 is : "+numIndex)
+
+
+
+
+
+
+
+
+
+
+
+
+					//CHAPTER 21 To 25
+					//TASK6
+
+
+
+  // var firstName = prompt("Write Your First Name","First Name")
+  // var lastName = prompt("Write Your Last Name","Last Name")
+  // var thirdString = " Have a nice day!";
+  // var result = firstName.concat(lastName, thirdString);
+  // alert(result)
+
+
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK7
+
+
+
+// var hyder="hyderabad"
+// var islam=hyder.replace("hyder","islam")
+// alert(islam)
+
+
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK8
+
+// var message = "Ali and Sami are best friends. They play cricket andfootball together."
+// var rep=message.replace(/and/g,"&")
+//  alert(rep)
+
+
+
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK9
+
+// var value=110
+// var type=value.toString();
+// document.write("Type : String")
+// document.write("<br>")
+// document.write("Value : "+value)
+
+
+
+
+// document.write("<br>")
+// num1=parseInt(value)
+// document.write("Type : Number")
+// document.write("<br>")
+// document.write("Value : "+num1)
+
+
+
+
+
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK10
+
+
+// var lower=prompt("Enter Your Name To make it Lower Case : ")
+// var upper=lower.toUpperCase()
+// document.write(upper)
+
+
+
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK11
+
+// var name=prompt("Please Enter Any Name","example")
+// var firstchar=name.slice(0,1)
+// var otherchar=name.slice(1)
+// otherchar=otherchar.toLowerCase()
+// firstchar=firstchar.toUpperCase()
+// var name=firstchar+otherchar
+// alert(name)
+
+
+
+
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK12
+
+// var number=365.32
+// var to=number.toString()
+// document.write("Number : "+number)
+// document.write("<br>")
+// document.write("String : "+to)
+
+
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK13
+
+
+// var userName=prompt("Enter a Name Please")
+// if (userName== String.fromCharCode(33) || userName== String.fromCharCode(44)|| userName== String.fromCharCode(46) || userName== String.fromCharCode(64)){
+// 	alert("Please Enter valid Name !")
+// }
+// alert(userName)
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK14
+
+
+
+// var product=prompt("Welcome to ABC Bakery What do you want Sir?")
+//  var upper=product.toLowerCase()
+// var bakery = ["cake", "apple pie", "cookie", "chips","patties"]
+// for (var i=0; i<bakery.length ; i++)
+// {
+// 	if(bakery[i]===upper){
+
+// 		alert(product+" is Available at index "+i+" in Our Bakery")
+// 		break;
+// 	}
+// }
+
+
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK16
+
+// var university = "University of Karachi"
+// var br="<br>"
+// var arr=university.split('')
+// console.log(arr)
+
+
+
+
+  					//CHAPTER 21 To 25
+					//TASK17
+
+
+// var name=prompt("Enter NAme It will Give You Last char ")
+// var har=name.charAt(name.length-1)
+// alert(har)
+
+
+  					//CHAPTER 21 To 25
+					//TASK18
+
+///I am Sorry Ye Prob Mujhhy samjh nahii ayaa
+
+
+
+
+
+
+
+
+
+
+
+  					//CHAPTER 26 To 30
+					//TASK1
+
+
+// var mNum=prompt("Enter a Positive Number ")
+// document.write("Number : "+mNum)
+// document.write("<br>")
+// var round=Math.round(mNum)
+// document.write("Round Of Value : "+round)
+// document.write("<br>")
+// var floo = Math.floor(mNum);
+// document.write("Floor Value"+floo)
+// document.write("<br>")
+// var ceil = Math.ceil(mNum);
+// document.write("Ceil Value"+ceil)
+
+
+
+
+
+  					//CHAPTER 26 To 30
+					//TASK2
+
+
+// var mNum=prompt("Enter a Negative Number ")
+// document.write("Number : "+mNum)
+// document.write("<br>")
+// var round=Math.round(mNum)
+// document.write("Round Of Value : "+round)
+// document.wrdocument.write("Ceil Value"+ceil)ite("<br>")
+// var floo = Math.floor(mNum);
+// document.write("Floor Value"+floo)
+// document.write("<br>")
+// var ceil = Math.ceil(mNum);
+
+
+
+
+
+
+
+  					//CHAPTER 26 To 30
+					//TASK3
+
+// var a=prompt("Please Enter a Number to Know The Absolute Value");
+// var absValue=Math.abs(a)
+// document.write("Absolute Value Of "+a+" is "+absValue)
+
+
+
+
+
+  					//CHAPTER 26 To 30
+					//TASK4
+
+ // var bigDecimal = Math.random();
+ // var improvedNum = (bigDecimal * 6) + 1;
+ // var numberOfStars = Math.floor(improvedNum);
+ // alert("random dice value is : "+numberOfStars)
+
+
+
+
+
+
+
+  					//CHAPTER 26 To 30
+					//TASK5
+
+
+//  var bigDecimal = Math.random();
+//  var improvedNum = (bigDecimal * 2) + 1;
+//  var numberOfStars = Math.floor(improvedNum);
+//  if (numberOfStars==2){
+
+//  alert(" random dice Coin is : Head ")
+// }
+// else{
+// 	 alert(" random Coin value is : Tail")
+// }
+
+
+
+
+
+
+
+
+
+  					//CHAPTER 26 To 30
+					//TASK6
+
+ // var bigDecimal = Math.random();
+ // var improvedNum = (bigDecimal * 100) + 1;
+ // var numberOfStars = Math.floor(improvedNum);
+ // alert("random number between 1 and 100 is : "+numberOfStars)
+
+
+
+
+
+  					//CHAPTER 26 To 30
+					//TASK7
+
+
+// var a=prompt("Enter your weight in kilograms")
+// var kgs=parseFloat(a)
+// alert("The weight of user is "+kgs+" kilograms")
+
+
+
+
+  					//CHAPTER 26 To 30
+					//TASK8
+
+// var a=prompt("Please Enter a Number between 1 to 10 : ")
+// var randomNum=Math.random();
+// var secretNum=(randomNum*10)+1
+// secretNum1=Math.floor(secretNum)
+// document.write(secretNum1)
+// if ( a == secretNum1) {
+// 	alert("Congratulations You Won! ")
+
+// }
+// else{
+// 	alert("Try Again !")
+// }
+
+
+
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK1
+
+
+// var rightNow = new Date();
+// document.write(rightNow)
+
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK2
+// var rightNow = new Date()
+// var months= ['January','February','March','April','May','June','July','August','September','October','November','December'];
+// var month = rightNow. getMonth()
+// document.write(months[month])
+
+
+
+
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK3
+
+// var a= new Date();
+// var b=a.toString();
+// var c=b.slice(0,3)
+// document.write("Today is : "+c)
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK4
+
+// var rightNow = new Date()
+// var day = rightNow.getDay()
+// if(day==0 || day == 6){
+// 	alert("its Fun Day !")
+// }
+
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK5
+
+// var rightNow = new Date();
+// var date = rightNow.getDate()
+// if(date <= 15){
+// 	alert("First Fifteen Days Of Month")
+// }
+// else{
+// 	alert("Last days of the month")
+// }
+
+
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK6
+
+
+// var rightNow = new Date();
+// var milliSec= rightNow.getTime();
+// var minutes = milliSec/60000
+// document.write("Current Date : "+rightNow)
+// document.write("<br>")
+// document.write("Ellapsed Milliseconds Since 1 , January , 1970  : "+milliSec)
+// document.write("<br>")
+// document.write("Ellapsed Minutes Since 1 , January , 1970 : "+minutes)
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK7
+// var rightNow = new Date();
+// var hours= rightNow.getHours();
+// if (hours >=0 || hours<=12) {
+//  alert("It's A:M")
+// }
+// else{
+// 	alert("It's P:M")
+// }
+// document.write()
+
+
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK 8
+// var targetDate = new Date();
+// targetDate.setDate(targetDate.getDate() + 10)
+// targetDate.setMonth(targetDate.getMonth() + 6);
+// document.write(targetDate)
+
+
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK 9
+ 
+// var date1 = new Date("04/24/2020"); 
+// var date2 = new Date("06/22/2020"); 
+// var calc = date2.getTime() - date1.getTime();
+// var ramzanDays = calc / (1000 * 3600 * 24); 
+// alert(ramzanDays+" days have passed Since 1st ramzan 2020") 
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK 10
+
+
+// var date1 = new Date("01/01/2015"); 
+// var date2 = new Date("06/21/2020"); 
+// var calc = date2.getTime() + date1.getTime();
+// var seconds = calc / 1000 
+// alert(" On Refrence Date "+date2+ " " +seconds+ " seconds had passed Since Beginning of 2015" ) 
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK 11
+
+
+// var e= new Date();
+// var d = new Date();
+// d.setHours(d.getHours() - 1);
+// document.write("Current Time is "+e)
+// document.write("<br>")
+// document.write("1 hour ago it Was "+d)
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK 12
+
+// var e= new Date();
+// var d = new Date();
+// d.setFullYear(d.getFullYear() - 100);
+// document.write("Current Date is "+e)
+// document.write("<br>")
+// document.write("100 years back it was "+d)
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK 13
+
+// var dob = new Date(prompt("Enter adte of birth","feb 02, 2001")) 
+// var dobMilli = dob.getTime()
+// var today=new Date()
+// var todayMilli=today.getTime()
+// var diff = todayMilli-dobMilli;
+// var accuage=diff/(1000*60*60*24*30*12)
+// accuage=Math.floor(accuage)
+// document.write("Your Date Of Birth is : "+dob.getFullYear())
+// document.write("<br>")
+// document.write("Your age is : "+accuage)
+
+
+
+
+
+  					//CHAPTER 31 To 34
+					//TASK 14
+// var nameCustomer = prompt("Enter Customer Name : ")
+// var monthName= prompt("Enter Month : ")
+// var units= prompt("Enter Number Of Units : ")
+// var chargesPerUnit=16
+// var lateAmount=350
+// var netAmount=chargesPerUnit*units
+// var lateAmountBill=chargesPerUnit*units+lateAmount
+// document.write("Customer Name : "+nameCustomer)
+// document.write("<br>")
+// document.write("Month : "+monthName)
+// document.write("<br>")
+// document.write("Number of Units : "+units)
+// document.write("<br>")
+// document.write("Charges per Units : "+chargesPerUnit)
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("Net Amount Payable Within (Due Date) : "+netAmount)
+// document.write("<br>")
+// document.write("Late Amount Charges : "+lateAmount)
+// document.write("<br>")
+// document.write("Gross Amount Payable After (Due Date) : "+lateAmountBill)
+// document.write("<br>")
+
+
+
+
+
+
+
+  					//CHAPTER 35 To 38
+					//TASK 1
+// function date(){
+// 	var today = new Date();
+// 	document.write(today)
+// }
+// date();
+
+
+
+
+  					//CHAPTER 35 To 38
+					//TASK 2
+
+// function greeting(){
+// 	var name1=prompt("Enter your First name please")
+// 	var name2=prompt("Enter your Last name please")
+// 	var name3= name1+name2
+// 	document.write("Assalam O Alaikum "+name3)
+// }
+// greeting();
+
+
+
+
+  					//CHAPTER 35 To 38
+					//TASK 3
+// function add(){
+// 	var a=5,b=10;
+// 	var z=a+b
+	
+// 	alert(z)
+// }
+// add();
+
+
+
+  					//CHAPTER 35 To 38
+					//TASK 4
+
+// function cal(num1,operator,num2) {
+// 	if(operator==="+"){
+// 		return num1+num2
+// 	}
+// 	else if(operator==="-"){
+// 		return num1-num2
+// 	}
+// 	else if(operator==="*"){
+// 		return num1*num2
+// 	}
+
+// }
+// var result1 =cal(10,"+",20)
+// var result2 =cal(10,"-",20)
+// var result3 =cal(10,"*",20)
+// document.write(result1)
+// document.write("<br>")
+// document.write(result2)
+// document.write("<br>")
+// document.write(result3)
+// document.write("<br>")
+
+
+
+
+  					//CHAPTER 35 To 38
+					//TASK 5
+
+// function square(a,b){
+// 		return (Math.pow(a,b));
+// }
+
+// var result=square(10,5)
+// document.write(result)
+
+
+
+
+
+
+
+
+  					//CHAPTER 35 To 38
+					//TASK 6
+
+
+
+
+
+// function factorial(n){
+//   let answer = 1;
+//   if (n == 0 || n == 1){
+//     return answer;
+//   }else{
+//     for(var i = n; i >= 1; i--){
+//       answer = answer * i;
+//     }
+//     return answer;
+//   }  
+// }
+// let n = 4;
+// answer = factorial(n)
+// document.write("The factorial of " + n + " is " + answer);
+
+
+
+
+
+
+
+
+
+
+
+
+  					//CHAPTER 35 To 38
+					//TASK 7
+// function counting(){
+// 	var a=prompt("Enter Sarting Num ")
+// 	var c=parseInt(a)
+// 	var b=prompt("Enter ending Num ")
+// 	var d=parseInt(b)
+// 	for(var i = c; i<=d; i++){
+// 		document.write(i)
+// 		document.write("<br>")
+// 	}
+// }
+// counting();
+
+
+
+
+
